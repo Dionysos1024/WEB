9128f4be2c697d0826798d79ead8be620aa6c1ae
evbbnhj84-cpu <evbbnhj84@gmail.com>
2026-07-27 21:57:33 +0900

    연습

--- 웹디자인개발기능사/따라치기_연습/E/JS/E-2.js
+++ 웹디자인개발기능사/따라치기_연습/E/JS/E-2.js
@@ -3,9 +3,10 @@ $(function() {
     $(".main-menu li").on("mouseover", function(){
         $(this).find(".sub-menu").stop().slideDown()
     })
-    $(".main-menu li").on("mouseout", function() {
+    $(".main-menu li").on("mouseout", function(){
         $(this).find(".sub-menu").stop().slideUp()
     })
+
 
 
 
@@ -13,18 +14,19 @@ $(function() {
     const time = 3000;
     const $slide = $(".slide")
     const $container = $(".slide-container")
-    const size = $slide.width();
+    const size = $slide.height();
     const count = $slide.length;
-    $container.width(size * count)
+    $container.height(size * count)
 
     setInterval(function() {
         $container.animate({
-            left: -size
+            top: -size
         }, speed, function() {
-            $container.css('left','0')
-            $container.append($(".slide").first())
+            $container.css('top','0')
+            $container.append($(".slide").first());
         });
     }, time);
+
 
 
 
@@ -35,4 +37,5 @@ $(function() {
     $(".close").on("click", function() {
         $("#modal").hide();
     });
+    
 })
