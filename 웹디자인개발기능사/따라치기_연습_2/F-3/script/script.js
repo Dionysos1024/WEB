7cc32566add568f8b79d7f554f5dcf03826337ee
evbbnhj84-cpu <evbbnhj84@gmail.com>
2026-08-11 22:28:36 +0900

    망했다

--- 웹디자인개발기능사/따라치기_연습_2/F-3/script/script.js
+++ 웹디자인개발기능사/따라치기_연습_2/F-3/script/script.js
@@ -9,26 +9,27 @@ $(function() {
     })
 
 
-    let index = 0
-    const speed = 500
+    const speed = 500;
     const time = 2000;
     const $slide = $(".slide")
     const $container = $(".slide-container")
-    const size = $slide.width()
-    const count = $slide.length
+    const size = $slide.width();
+    const count = $slide.length;
     $container.width(size * count)
 
     setInterval(function() {
-        index = (index + 1) % count
         $container.animate({
-            left: -index * size
-        }, speed)
-    }, time)
+            left: -size
+        }, speed, function() {
+            $container.css('left','0')
+            $container.append($(".slide").first());
+        } );
+    }, time);
 
 
 
     $(".menu-item").on("click", function() {
-        $(".container-item").toggle()
+        $(".content-item").toggle()
         $(".menu-item").toggleClass("active")
     })
 
