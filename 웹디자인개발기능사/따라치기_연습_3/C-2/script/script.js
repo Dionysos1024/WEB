fedccaea5ebf629429f18f53cececd4437ad89d7
evbbnhj84-cpu <evbbnhj84@gmail.com>
2026-08-21 21:24:19 +0900

    피곤해

--- 웹디자인개발기능사/따라치기_연습_3/C-2/script/script.js
+++ 웹디자인개발기능사/따라치기_연습_3/C-2/script/script.js
@@ -4,8 +4,10 @@
 
 /* 확인 및 수정 작업 퇴근후 집에서 할것! */
 
+/* 선생님 코드 보고, 다시 따라치기 시작 21시 16분 완료 21시 24분 */
 
-$(function() {
+
+/* $(function() {
 
     $(".main-menu li").on("mouseover", function() {
         $(this).find(".sub-menu").slideDown()
@@ -31,4 +33,39 @@ $(function() {
     $(".btn-modal").on("click", function() {
         $("#modal").toggle()
     })
+}) */
+
+
+
+$(function() {
+
+    $(".main-menu li").on("mouseover", function(){
+        $(this).find(".sub-menu").stop().slideDown()
+    })
+    $(".main-menu li").on("mouseout", function(){   
+        $(this).find(".sub-menu").stop().slideUp()
+    })
+    
+
+
+    let index = 0;
+    const speed = 500;
+    const time = 2000;
+    const $slide = $(".slide")
+    const count = $(".slide").length
+    $slide.not(":first").hide()
+    
+    setInterval(function() {
+        index = (index + 1) % count;
+        $slide.fadeOut(speed)
+        $slide.eq(index).fadeIn(speed)
+        $slide.css("z-index", "-1")
+    }, time)
+    
+
+
+    $(".btn-modal").on("click", function() {
+        $("#modal").toggle()
+    })
+
 })
