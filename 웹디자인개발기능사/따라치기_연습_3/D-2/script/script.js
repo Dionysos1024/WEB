de587885360c5f92f581bae00a18a2e0ce02cdad
Dionysos1024 <evbbnhj84@gmail.com>
2026-08-24 17:27:39 +0900

    css멘탈탈곡기

---
+++ 웹디자인개발기능사/따라치기_연습_3/D-2/script/script.js
@@ -0,0 +1,43 @@
+
+
+/* 시작 : 08월 24일 17시 05분 / 종료 : 17시 15분
+        총 작성 시간 10분 / 확인 및 수정 00분 소요 */
+
+/* 틀린 부분 : 00개 */
+
+
+$(function() {
+
+        // 개별 메뉴 슬라이드
+        $(".main-menu li").on("mouseover", function() {
+                $(this).find(".sub-menu").stop().slideDown()
+        })
+        $(".main-menu li").on("mouseout", function() {
+                $(this).find(".sub-menu").stop().slideUp()
+        })
+
+
+        // 가로 슬라이드
+        const speed = 500
+        const time = 2000
+        const $slide = $(".slide")
+        const $container = $(".slide-container")
+        const size = $slide.width()
+        const count = $slide.length
+        $container.width(size * count)
+
+        setInterval(() => {
+                $container.animate({
+                        left: -size
+                }, speed, function() {
+                        $container.css('left','0')
+                        $container.append($($slide).first())
+                })
+        }, time);
+
+
+        // 레이어 팝업
+        $(".btn-modal").on("click", function() {
+                $("#modal").toggle()
+        })
+})
