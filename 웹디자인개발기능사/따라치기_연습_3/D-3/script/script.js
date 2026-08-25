7c534be3861604142f3de6960fb81da3e9db868b
evbbnhj84-cpu <evbbnhj84@gmail.com>
2026-08-25 22:57:27 +0900

    때려치울까

--- 웹디자인개발기능사/따라치기_연습_3/D-3/script/script.js
+++ 웹디자인개발기능사/따라치기_연습_3/D-3/script/script.js
@@ -5,6 +5,43 @@
 
 /* 최대한 시간 안따지고 천천히 생각해서 해볼생각임, 목표 틀리거나 빼먹은거 10개 이하 */
 
-/* 시작 시간 : 08월 25일 00시 00분 / 종료 시간 : 00시 00분 */
+/* 시작 시간 : 08월 25일 22시 30분 / 종료 시간 : 22시 40분 */
+
+/* 틀린 부분 : 04줄 / 수정 완료 22시 58분 */
 
 
+$(function() {
+
+    $(".main, .sub").on("mouseenter", function() {
+        $(".sub").stop().fadeIn()
+    })
+    $(".main, .sub").on("mouseleave", function() {
+        $(".sub").stop().fadeOut()
+    })
+
+
+    let index = 0
+    const speed = 500
+    const time = 2000
+    const $slide = $(".slide")
+    const count = $(".slide").length
+    $slide.not(":first").hide()
+    
+    setInterval(function() {
+        index = (index + 1) % count;
+        $slide.fadeOut(speed)
+        $slide.eq(index).fadeIn(speed)
+    }, time)
+
+
+    $(".menu-item").on("click", function() {
+        $(".content-item").toggle()
+        $(".menu-item").toggleClass("active")
+    })
+
+
+    $(".btn-modal").on("click", function() {
+        $("#modal").toggle()
+    })
+
+})
