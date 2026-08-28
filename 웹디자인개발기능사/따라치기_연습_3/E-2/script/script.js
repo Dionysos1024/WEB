7055d045eb38606fb4895eeae4c304135c74cd59
Dionysos1024 <evbbnhj84@gmail.com>
2026-08-28 20:08:31 +0900

    포기가눈앞에아른아른

---
+++ 웹디자인개발기능사/따라치기_연습_3/E-2/script/script.js
@@ -0,0 +1,36 @@
+
+/* 08월 28일 / 시작 : 17시 05분 / 종료 : 17시 13분 */
+
+/* 확인 및 수정 : 틀린 부분 00개 */
+/* 메뉴 슬라이드가 동작 안됨, css 문제로 판단, css 검토 후 다시 확인 할것! */
+
+$(function() {
+
+    $(".main-menu li").on("mouseover", function() {
+        $(this).find(".sub-menu").stop().slideDown()
+    })
+    $(".main-menu li").on("mouseout", function() {
+        $(this).find(".sub-menu").stop().slideUp()
+    })
+
+    const speed = 500
+    const time = 2000
+    const $slide = $(".slide")
+    const $container = $(".slide-container")
+    const size = $slide.height()
+    const count = $slide.length
+    $container.height(size * count)
+
+    setInterval(() => {
+        $container.animate({
+            top: -size
+        }, speed, function() {
+            $container.css('top','0')
+            $container.append($($slide).first())
+        })
+    }, time);
+
+    $(".btn-modal").on("click", function() {
+        $("#modal").toggle()
+    })
+})
